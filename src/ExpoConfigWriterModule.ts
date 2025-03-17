15ccc776fefd42d293fed3a333bc47cccfc09178
Lane Lu <161492667+llucn@users.noreply.github.com>
2025-03-17 20:25:21 +0800

    feat: add podfile content

--- src/ExpoConfigWriterModule.ts
+++ src/ExpoConfigWriterModule.ts
@@ -1,12 +1,6 @@
 import { NativeModule, requireNativeModule } from 'expo';
 
-import { ExpoConfigWriterModuleEvents } from './ExpoConfigWriter.types';
-
-declare class ExpoConfigWriterModule extends NativeModule<ExpoConfigWriterModuleEvents> {
-  PI: number;
-  hello(): string;
-  setValueAsync(value: string): Promise<void>;
+declare class ExpoConfigWriterModule extends NativeModule {
 }
 
-// This call loads the native module object from the JSI.
 export default requireNativeModule<ExpoConfigWriterModule>('ExpoConfigWriter');
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,1 @@
-// Reexport the native module. On web, it will be resolved to ExpoConfigWriterModule.web.ts
-// and on native platforms to ExpoConfigWriterModule.ts
-export { default } from './ExpoConfigWriterModule';
-export { default as ExpoConfigWriterView } from './ExpoConfigWriterView';
-export * from  './ExpoConfigWriter.types';
+import ExpoConfigWriterModule from './ExpoConfigWriterModule';
